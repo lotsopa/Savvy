b3ac6bd25e9f3fa49f74ec619f09eabd79ccf88e
MentalDude <apostol_dadachev@yahoo.com>
2015-05-22 20:10:53 +0200

    Added License information to all header files.

--- Savvy/inc/SavvyFramework.h
+++ Savvy/inc/SavvyFramework.h
@@ -1,10 +1,35 @@
 /*
+The MIT License (MIT)
+
+Copyright (c) 2015 Apostol Dadachev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef SAVVY_FRAMEWORK_H
+#define SAVVY_FRAMEWORK_H
+
+/*
 The main Savvy Framework header file.
 This is the ONLY header file you need to include in your application if you only want to extend the framework
 and do not wish to use the provided out of the box conversions.
 */
-#ifndef SAVVY_FRAMEWORK_H
-#define SAVVY_FRAMEWORK_H
 
 #include "SavvyPCH.h"
 #include "SavvyFrameworkDefines.h"
